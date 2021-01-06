3155f25a16e8b7808e16a7379214bdb8e9d18def
Mike Bastakis <csd4406@csd.uoc.gr>
2021-01-06 21:49:46 +0200

    Java

---
+++ HY252/PhaseA_4406/src/main/Amphipolis.java
@@ -0,0 +1,12 @@
+package main;
+import view.View;
+
+public class Amphipolis {
+
+    public static void main(String[] args) {
+
+        View game = new View();
+
+    }
+
+}
--- HY252/PhaseA_4406/src/view/View.java
+++ HY252/PhaseA_4406/src/view/View.java
@@ -2,11 +2,14 @@ package view;
 
 import controller.Controller;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
 
 /**
  * A class that extends JFrame class and is responsible for Graphical Interface .
@@ -32,7 +35,29 @@ public class View extends JFrame {
      * Creates a window and showcases the start point of the board,
      * by initializing panels,buttons.
      */
-    public View() {
+    public View(){
+        // Set default configuration for the window.
+        this.setTitle("Amphipolis");
+        this.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+        this.setLayout(null);
+        //this.setResizable(false);
+        this.setSize(1200,1000);
+        // Set Icon for window.
+        this.setIconImage(new ImageIcon(this.getClass().getResource("/images/tile_back.png")).getImage());
+        // Main Jpannel
+        main = new JPanel();
+        Border b =  BorderFactory.createLineBorder(Color.GREEN,3);
+        JLabel label = new JLabel();
+        ImageIcon icon = new ImageIcon(new ImageIcon(this.getClass().getResource("/images/background.png")).getImage().getScaledInstance(900,750,Image.SCALE_SMOOTH));
+        label.setIcon(icon);
+        label.setBounds(0,0,900,750);
+        label.setBorder(b);
+
+
+        main.add(label);
+        main.setBounds(-150,0,1200,1000);
+        this.add(main);
+        this.setVisible(true);
     }
 
     /**
@@ -56,8 +81,8 @@ public class View extends JFrame {
     public class myDesktopPane extends JDesktopPane {
         private Image backImage = null;
 
-        public myDesktopPane() {
-
+        public myDesktopPane(Image image) {
+            backImage = image;
         }
         /**
          * <b>Transformer</b>
@@ -66,14 +91,14 @@ public class View extends JFrame {
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            g.drawImage(image, 0, 0, this);
+            g.drawImage(backImage, 0, 0, this);
         }
     }
     /**
      * <b>Listener</b>
      * Perform action when a tile in an area of the board is clicked.
      * */
-    public class getTile implements ActionListener{
+    public class getTile implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
 
